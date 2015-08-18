e6c992c810449219ca8e5c79e1c536f78d0480cc
Oliver Flasch <of@sourcewerk.de>
2015-08-18 21:00:31 +0200

    updated authors

--- skel/R/simplr.R
+++ skel/R/simplr.R
@@ -1,7 +1,9 @@
 ## simplr.R
 ##
 ## SimplR - Basic Symbolic Expression Simplification
-## 2015 sourcewerk UG, Oliver Flasch (oliver.flasch@sourcewerk.de)
+## 2015 sourcewerk UG
+## Oliver Flasch (of@sourcewerk.de)
+## Felix Gorschlueter (fg@sourcewerk.de)
 ## All rights reserved. 
 ##
 
